3229d2b08f0b06003af400e0e0e9b90119b4b40f
dipesh <kcdipesh429@gmail.com>
2020-12-08 15:28:43 +0545

    feat:add continous user input and refactor

--- main.go
+++ main.go
@@ -13,33 +13,47 @@ import (
 	"github.com/sec51/twofactor"
 )
 
+var (
+	email  = "dipesh.kc@wesionary.team"
+	issuer = "wesionaryTEAM"
+)
+
+func getUserInput() string {
+	//Get User Inputs(token)
+	reader := bufio.NewReader(os.Stdin)
+	text, _ := reader.ReadString('\n')
+	return strings.Replace(text, "\n", "", -1)
+
+}
+
 func main() {
 	//Instantiate totp
-	otp, err := twofactor.NewTOTP("kcdipesh429@gmail.com", "dipesh", crypto.SHA1, 6)
+	otp, err := twofactor.NewTOTP(email, issuer, crypto.SHA1, 6)
 	if err != nil {
-		log.Print("Error during OTP generation", err)
+		log.Print("Error during OTP generation: ", err)
 	}
 	// Generate QR bytes
 	qrBytes, err := otp.QR()
 	if err != nil {
-		log.Print("Error during QR generation", err)
+		log.Print("Error during QR generation: ", err)
 	}
 	//Generate PNG from QR bytes
 	img, _, _ := image.Decode(bytes.NewReader(qrBytes))
 	out, err := os.Create("./QRImg.png")
 	err = png.Encode(out, img)
 
-	//Get User Inputs(token)
-	reader := bufio.NewReader(os.Stdin)
-	text, _ := reader.ReadString('\n')
-	text = strings.Replace(text, "\n", "", -1)
+	for {
+		log.Print("Enter OTP Token")
+		text := getUserInput()
+		//Validate the token
+		if err = otp.Validate(text); err != nil {
+			log.Print("Error during Validaion: ", err)
 
-	//Validate the token
-	if err = otp.Validate(text); err != nil {
-		log.Print("Error during Validaion", err)
-	} else {
+		} else {
 
-		log.Print("Validation Success")
+			log.Print("Validation Success")
+			break
+		}
 	}
 
 }
